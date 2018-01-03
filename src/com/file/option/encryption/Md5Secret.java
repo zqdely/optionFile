373cf989aee61fe259e635d8861456293e9c14c5
Ally <zqdely@163.com>
2018-01-03 22:15:00 +0800

    MD5加密

---
+++ src/com/file/option/encryption/Md5Secret.java
@@ -0,0 +1,45 @@
+package com.file.option.encryption;
+
+
+import java.security.MessageDigest;
+
+public class MD5Secret { 
+	/**
+	 * MD5加码 生成32位md5码(李迎)
+	 * @param inStr
+	 * @return
+	 */
+    public static String string2MD5(String inStr){  
+        MessageDigest md5 = null;  
+        try{  
+            md5 = MessageDigest.getInstance("MD5");  
+        }catch (Exception e){  
+            System.out.println(e.toString());  
+            e.printStackTrace();  
+            return "";  
+        }  
+        char[] charArray = inStr.toCharArray();  
+        byte[] byteArray = new byte[charArray.length];  
+  
+        for (int i = 0; i < charArray.length; i++)  
+            byteArray[i] = (byte) charArray[i];  
+        byte[] md5Bytes = md5.digest(byteArray);  
+        StringBuffer hexValue = new StringBuffer();  
+        for (int i = 0; i < md5Bytes.length; i++){  
+            int val = ((int) md5Bytes[i]) & 0xff;  
+            if (val < 16)  
+                hexValue.append("0");  
+            hexValue.append(Integer.toHexString(val));  
+        }  
+        return hexValue.toString();
+    }  
+    
+    
+    public static void main(String[] args) {
+		
+    	String str1 = "张丽";
+		
+		System.out.println(string2MD5(str1));
+    	
+	}
+}
